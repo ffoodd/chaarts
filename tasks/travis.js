6bb41be80535ca1efcd0634885aa4368367da6cd
Gaël Poupard <gael.poupard@orange.com>
2019-06-13 14:23:13 +0200

    chore: update Travis task for #10

--- tasks/travis.js
+++ tasks/travis.js
@@ -1,18 +1,42 @@
 const fs       = require('fs');
 const gulp     = require('gulp');
+const options  = require('./options');
 const html     = require('html-validator');
 const eslinter = require('gulp-eslint');
 const linter   = require('gulp-stylelint');
-const options  = require('./options');
+const symbols  = require('log-symbols');
+const chalk    = require('chalk');
+
+function displayTest(test) {
+  switch(test.status) {
+    case 'error':
+    case false:
+      var symbol = symbols.error;
+      break;
+    case 'warning':
+      var symbol = symbols.warning;
+      break;
+    case 'info':
+      var symbol = symbols.info;
+      break;
+    case true:
+    default:
+      var symbol = symbols.success;
+  }
+
+  console.error(chalk`${symbol} {white ${test.name}} ${test.value}`)
+};
 
 function travisHTML(done) {
-  fs.readFile(options.test.home, 'utf8', (error, response) => {
+  for (let page in options.test.arr) {
+    fs.readFile(options.test.arr[page], 'utf8', (error, response) => {
     if (error) {
       throw error;
     }
 
     html({data: response})
       .then((data) => {
+        console.log(chalk.cyan(`File to test: ${options.test.arr[page]}`))
         let messages = JSON.parse(data);
         let errors = [];
         let results  = messages.messages.reduce((results, value, key) => {
@@ -37,6 +61,7 @@ function travisHTML(done) {
         console.error(error)
       })
     });
+  }
 
   done();
 }
